5d8f5a67ce179e8ed8dca402a82fccd088956249
Bob Ershov <raveman@raveman.ru>
2016-02-07 14:05:32 +0300

    Initial Commit

---
+++ YouTubeDL_UI/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  YouTubeDL_UI
+//
+//  Created by Bob Ershov on 07/02/16.
+//  Copyright © 2016 Bob Ershov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ YouTubeDL_UI/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  YouTubeDL_UI
+//
+//  Created by Bob Ershov on 07/02/16.
+//  Copyright © 2016 Bob Ershov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
